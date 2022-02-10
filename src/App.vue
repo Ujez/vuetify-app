9cd2d82bdbfd930fdc8d7f228e250c0a6619838b
ujez <ujezujez@gmail.com>
2022-02-10 14:13:02 +0100

    now the link btns are dynamic

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,14 @@
     <v-app-bar app color="primary" dark>
       <v-toolbar-title> Vuetify Dashboard</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn text rounded to="/">Home</v-btn>
-        <v-btn text rounded to="/login">Login</v-btn>
+        <v-btn 
+         v-for="link in links" 
+         :key="`${link.label}-header-link`"
+        text 
+        rounded 
+        :to="link.url">
+        {{ link.label }}
+        </v-btn>
     </v-app-bar>
     <v-main>
      <router-view></router-view>
