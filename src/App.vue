6ae03bbd7168b27114c992b0b305c5c4aab56e4e
ujez <ujezujez@gmail.com>
2022-02-14 07:00:03 +0100

    completed the requirements for the signup page

--- src/App.vue
+++ src/App.vue
@@ -67,6 +67,10 @@ export default {
        url: '/login'
      },
      {
+       label: 'Signup',
+       url: '/signup'
+     },
+     {
        label: 'Dashboard',
        url: '/dashboard'
      }
